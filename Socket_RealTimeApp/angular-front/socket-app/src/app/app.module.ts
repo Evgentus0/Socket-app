8908441e93858c00af35c7036bd20c059ab3f2e4
Yevgeniy Romanenko <zerom2016romanenko@gmail.com>
2019-12-05 01:59:41 +0200

    init project

---
+++ Socket_RealTimeApp/angular-front/socket-app/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import {FormsModule} from "@angular/forms";
+
+const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
+
+import { AppComponent } from './app.component';
+import { DocumentListComponent } from './document-list/document-list.component';
+import { DocumentComponent } from './document/document.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DocumentListComponent,
+    DocumentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    SocketIoModule.forRoot(config)
+  ],  
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Socket_RealTimeApp/angular-front/socket-app/src/app/document.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { Document } from 'D:/Education/Socket_RealTimeApp/angular-front/socket-app/src/app/document';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentService {
+  currentDocument = this.socket.fromEvent<Document>('document');
+  documents = this.socket.fromEvent<string[]>('documents');
+
+  constructor(private socket: Socket) { }
+
+  getDocument(id: string) {
+    this.socket.emit('getDoc', id);
+  }
+
+  newDocument() {
+    this.socket.emit('addDoc', { id: this.docId(), doc: '' });
+  }
+
+  editDocument(document: Document) {
+    this.socket.emit('editDoc', document);
+  }
+
+  deleteDocument(id: string){
+    this.socket.emit('deleteDoc', id);
+  }
+
+  private docId() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < 5; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+  }
+}
